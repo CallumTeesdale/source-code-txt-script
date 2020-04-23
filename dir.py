fe9ca180f60a1fa62389db8819c6bc4bf2c7ff37
callum <callumjamesteesdale@gmail.com>
2020-04-23 12:01:14 +0100

    command line arguments to name extensions and ignore folders

--- dir.py
+++ dir.py
@@ -5,8 +5,8 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser(description='Source code txt.maker')
-parser.add_argument("--e", nargs='+',  default=None, type=str, help="Supply file extensions")
-parser.add_argument("--i", nargs='+', default=None, type=str, help="Ignore folder")
+parser.add_argument("--e", nargs='+',  default=None, type=str, help="Supply file extensions to add to source file")
+parser.add_argument("--i", nargs='+', default=None, type=str, help="Ignore folder/file from source")
 for _, value in parser.parse_args()._get_kwargs():
     if _ == 'e':
         extension = value
@@ -14,18 +14,21 @@ for _, value in parser.parse_args()._get_kwargs():
     if _ == 'i':
         ignore = value
         print('Ignore'+str(ignore))
-for i in range(len(sys.argv) - 1):
-    print(sys.argv[i + 1])
-    for filepath in glob.glob('**/*' + sys.argv[i + 1], recursive=True):
+for i in range(len(extension)):
+    print(extension[i])
+    for filepath in glob.glob('**/*' + extension[i], recursive=True):
         print(filepath)
-        if os.path.isfile(filepath) and filepath != "source.txt" and ignore[0] not in filepath:
-            a = open('source.txt', 'a+')
-            a.write("\n")
-            a.write("################################################################\n")
-            a.write(filepath + "\n")
-            a.write("################################################################\n")
-            a.write("\n")
+        if os.path.isfile(filepath) and filepath != "source.txt":
+            folders = filepath.split("/")
+            print("folders"+str(folders))
+            if not any(x in folders for x in ignore):
+                a = open('source.txt', 'a+')
+                a.write("\n")
+                a.write("################################################################\n")
+                a.write(filepath + "\n")
+                a.write("################################################################\n")
+                a.write("\n")
 
-            with open(filepath, "r") as file:
-                for line in file:
-                    a.write(line + "\n")
+                with open(filepath, "r") as file:
+                    for line in file:
+                        a.write(line)
